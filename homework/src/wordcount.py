e020151c3f29e6e6189102cfeb8b8902c14068fb
jotorresbe <jotorresbe@unal.edu.co>
2025-11-22 11:11:23 -0500

    test homework funcional

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,16 +1,23 @@
 # obtain a list of files in the input directory
-
+import sys
 from homework.src._internals.preprocess_lines import preprocess_lines
 from homework.src._internals.read_all_lines import read_all_lines
 from homework.src._internals.split_into_words import split_into_words
 from homework.src._internals.count_words import count_words
 
-from ._internals.write_count_words import write_count_words
+from ._internals.write_word_counts import write_count_words
 
 
 def main():
-    input_folder = "data/input/"
-    output_folder = "data/output/"
+
+    if len(sys.argv) !=3:
+        print("Usage: python -m homework <input_folder> <output_folder>")
+        sys.exit(1)
+
+    input_folder = sys.argv[1]
+    output_folder = sys.argv[2]
+
+    # para usar de comando python -m homework data/input data/output
     all_lines = read_all_lines(input_folder) 
     all_lines = preprocess_lines(all_lines) 
     words = split_into_words(all_lines)
